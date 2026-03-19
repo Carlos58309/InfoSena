964efdeb71501953eaf63081c11b678c4e7aeb15
Carlos58309 <perezpolancocarlosmario@gmail.com>
2026-03-19 17:08:34 -0500

    imagenes

--- info/urls.py
+++ info/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,5 +33,6 @@ urlpatterns = [
     
 ]
 
-# Sirve media en desarrollo Y producción
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
